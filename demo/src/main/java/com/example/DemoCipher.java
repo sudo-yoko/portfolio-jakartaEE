bc0cbadb7b9511d4dc8036f137917ab2e780b5bc
sudou-y <83765620+sudo-yoko@users.noreply.github.com>
2025-04-02 15:15:19 +0900

    202504021515

--- demo/src/main/java/com/example/DemoCipher.java
+++ demo/src/main/java/com/example/DemoCipher.java
@@ -4,5 +4,12 @@ package com.example;
  * 暗号化と複合を行うユーティリティクラス
  */
 public class DemoCipher {
+    private static final String ALGORITHM = "AES";
+
+    public static String encrypt(String plaintext){
+        String aesKey = "16byte128bit----";
+        SecretKeySpec secretKey = new SecretKeySpec(aesKey.getByte)
+
+    }
 
 }
