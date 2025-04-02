ec14635cabea702393d57e36782bf9675751a466
sudou-y <83765620+sudo-yoko@users.noreply.github.com>
2025-04-02 06:37:19 +0000

    202504021537

--- demo/src/main/java/com/example/DemoCipher.java
+++ demo/src/main/java/com/example/DemoCipher.java
@@ -6,10 +6,26 @@ package com.example;
 public class DemoCipher {
     private static final String ALGORITHM = "AES";
 
-    public static String encrypt(String plaintext){
+    public static String encrypt(String plaintext) {
+        // AES鍵を取得
         String aesKey = "16byte128bit----";
-        SecretKeySpec secretKey = new SecretKeySpec(aesKey.getByte)
+        SecretKeySpec secretKey = new SecretKeySpec(aesKey.getByte(StandardCharsets.UTF_8), ALGORITHM);
 
+        byte[] unencrypted = null;
+        //try{
+         // 暗号化モードに初期化
+            Cipher cipher = Cipher.getInstance(ALGORITHM);
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+
+            // 平文をbyte配列にする
+            unencrypted = plaintext.getBytes(StandardCharsets.UTF_8);
+
+            // 暗号化する
+            byte[] encrypted = cipher.doFinal(unencrypted);
+
+            // 暗号化後のbyte配列をBase64エンコードして文字列の形式にする
+            //return Base64.getEncoder().
+       //}//
     }
 
 }
